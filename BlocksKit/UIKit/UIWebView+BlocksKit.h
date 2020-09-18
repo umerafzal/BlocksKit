0bd84961316f63f33d894d89ee90c596f43c1c50
Umer Afzal <umer.afzal07@gmail.com>
2020-09-18 15:13:45 +0500

    UIWebView removed + UIWebViewDelegate methods code commented out.

--- BlocksKit/UIKit/UIWebView+BlocksKit.h
+++ BlocksKit/UIKit/UIWebView+BlocksKit.h
@@ -5,6 +5,7 @@
 
 #import "BKDefines.h"
 #import <UIKit/UIKit.h>
+#import <WebKit/WebKit.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
@@ -13,23 +14,23 @@ NS_ASSUME_NONNULL_BEGIN
  @warning UIWebView is only available on a platform with UIKit.
 */
 
-@interface UIWebView (BlocksKit)
+@interface WKWebView (BlocksKit)
 
 /** The block to be decide whether a URL will be loaded. 
  
  @warning If the delegate implements webView:shouldStartLoadWithRequest:navigationType:,
  the return values of both the delegate method and the block will be considered.
 */
-@property (nonatomic, copy, setter = bk_setShouldStartLoadBlock:, nullable) BOOL (^bk_shouldStartLoadBlock)(UIWebView *webView, NSURLRequest *request, UIWebViewNavigationType navigationType);
+@property (nonatomic, copy, setter = bk_setShouldStartLoadBlock:, nullable) BOOL (^bk_shouldStartLoadBlock)(WKWebView *webView, NSURLRequest *request, UIWebViewNavigationType navigationType);
 
 /** The block that is fired when the web view starts loading. */
-@property (nonatomic, copy, setter = bk_setDidStartLoadBlock:, nullable) void (^bk_didStartLoadBlock)(UIWebView *webView);
+@property (nonatomic, copy, setter = bk_setDidStartLoadBlock:, nullable) void (^bk_didStartLoadBlock)(WKWebView *webView);
 
 /** The block that is fired when the web view finishes loading. */
-@property (nonatomic, copy, setter = bk_setDidFinishLoadBlock:, nullable) void (^bk_didFinishLoadBlock)(UIWebView *webView);
+@property (nonatomic, copy, setter = bk_setDidFinishLoadBlock:, nullable) void (^bk_didFinishLoadBlock)(WKWebView *webView);
 
 /** The block that is fired when the web view stops loading due to an error. */
-@property (nonatomic, copy, setter = bk_setDidFinishWithErrorBlock:, nullable) void (^bk_didFinishWithErrorBlock)(UIWebView *webView, NSError *error);
+@property (nonatomic, copy, setter = bk_setDidFinishWithErrorBlock:, nullable) void (^bk_didFinishWithErrorBlock)(WKWebView *webView, NSError *error);
 
 @end
 
