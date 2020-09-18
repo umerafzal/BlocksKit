705e31330e1c6d404da8a8dd609db8d9a8257a09
Umer Afzal <umer.afzal07@gmail.com>
2020-09-18 15:29:57 +0500

    UIWebView removed completely -- only category declaration available now

---
+++ BlocksKit/UIKit/WKWebView+BlocksKit.h
@@ -0,0 +1,22 @@
+//
+//  WKWebView+BlocksKit.h
+//  BlocksKit
+//
+
+#import "BKDefines.h"
+#import <UIKit/UIKit.h>
+#import <WebKit/WebKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+/** Block callbacks for WKWebView.
+
+ @warning WKWebView is only available on a platform with UIKit.
+*/
+
+@interface WKWebView (BlocksKit)
+
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ BlocksKit/UIKit/WKWebView+BlocksKit.m
@@ -0,0 +1,30 @@
+//
+//  WKWebView+BlocksKit.m
+//  BlocksKit
+//
+
+#import "WKWebView+BlocksKit.h"
+#import "A2DynamicDelegate.h"
+#import "NSObject+A2DynamicDelegate.h"
+#import "NSObject+A2BlockDelegate.h"
+
+#pragma mark Custom delegate
+
+@interface A2DynamicUIWebViewDelegate : A2DynamicDelegate
+@end
+
+@implementation A2DynamicUIWebViewDelegate
+
+@end
+
+#pragma mark Category
+
+@implementation WKWebView (BlocksKit)
+
+
++ (void)load
+{
+
+}
+
+@end
